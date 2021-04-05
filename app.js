f26960c3157c99f057db095d22ea651a7d331ce6
tglazman <totoglazman@gmail.com>
2021-04-05 19:11:05 -0300

    Version Final

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.listen(port, () => {
-    console.log('Mercado-Liebre listening on port '+ port);
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/ingresa', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Servidor corriendo en el puerto 3000');
 });
